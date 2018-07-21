715e1d41891d7e7b2eaed0956cf47b495437e2b8
xqgex <xqgex.n@gmail.com>
2018-07-21 17:49:48 +0300

    Work, Work, work...

--- Windows_PC/libCrazyGame.py
+++ Windows_PC/libCrazyGame.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import os,sys,time
 
-BOARD_SIZE_X = 10
-BOARD_SIZE_Y = 10
+BOARD_SIZE_X = 17
+BOARD_SIZE_Y = 7
 
 HUMAN_PLAYER = 0
 COMPUTER_PLAYER = 1
@@ -25,6 +25,9 @@ INFO_ELAPSED = 2
 # def updateInfo(info_type, new_value)
 #	Update the requested value at the CLI info box
 #
+# def drawPiece(board, old_board = None)
+#	Draw the drones position to the screen
+#
 # def key_handle()
 #	Get one char from the keyboard
 #
@@ -44,29 +47,29 @@ def drawBoxs(): # Print to the screen the frame of the CLI
 		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 6+2*indx, 2, "║{} ║".format(" │"*(BOARD_SIZE_X-1))))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 3+2*BOARD_SIZE_Y, 2, "╚{}═╝".format("═╧"*(BOARD_SIZE_X-1))))
 	# Game status
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 4, 4+2*BOARD_SIZE_X, "┏━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━┓"))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 5, 4+2*BOARD_SIZE_X, "┃ Game status  ┃               ┃"))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 6, 4+2*BOARD_SIZE_X, "┃ Total turns  ┃               ┃"))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 7, 4+2*BOARD_SIZE_X, "┃ Time elapsed ┃               ┃"))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 8, 4+2*BOARD_SIZE_X, "┗━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━┛"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 3, 4+2*BOARD_SIZE_X, "┏━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━┓"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 4, 4+2*BOARD_SIZE_X, "┃ Game status  ┃               ┃"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 5, 4+2*BOARD_SIZE_X, "┃ Total turns  ┃               ┃"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 6, 4+2*BOARD_SIZE_X, "┃ Time elapsed ┃               ┃"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 7, 4+2*BOARD_SIZE_X, "┗━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━┛"))
 	# Player 1
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (11, 4+2*BOARD_SIZE_X, "  Player 1   CrazyFlie: 1  "))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (12, 4+2*BOARD_SIZE_X, "             CrazyFlie: 2  "))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (13, 4+2*BOARD_SIZE_X, "             Targets:   3&4"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (10, 4+2*BOARD_SIZE_X, "  Player 1   CrazyFlie: 1  "))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (11, 4+2*BOARD_SIZE_X, "             CrazyFlie: 2  "))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (12, 4+2*BOARD_SIZE_X, "             Target:    3  "))
 	# player 2
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (17, 4+2*BOARD_SIZE_X, "  Player 2   CrazyFlie: A  "))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (18, 4+2*BOARD_SIZE_X, "             CrazyFlie: B  "))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (19, 4+2*BOARD_SIZE_X, "             Targets:   C&D"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (16, 4+2*BOARD_SIZE_X, "  Player 2   CrazyFlie: A  "))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (17, 4+2*BOARD_SIZE_X, "             CrazyFlie: B  "))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (18, 4+2*BOARD_SIZE_X, "             Target:    C  "))
 	# Flush
 	sys.stdout.flush()
 
 def changePlayerVisual(current_player): # Update the CLI when the current user has changed
 	if current_player == 1:
-		bolded_start = 9
-		regular_start = 15
+		bolded_start = 8
+		regular_start = 14
 	else:
-		bolded_start = 15
-		regular_start = 9
+		bolded_start = 14
+		regular_start = 8
 	# Bold the current player
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+1, 4+2*BOARD_SIZE_X, "█▀▀▀▀▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+2, 4+2*BOARD_SIZE_X, "█"))
@@ -75,7 +78,7 @@ def changePlayerVisual(current_player): # Update the CLI when the current user h
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+3, 4+2*BOARD_SIZE_X, "█ Current  █"))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+3, 4+2*BOARD_SIZE_X+len(" Current  █ CrazyFlie: X  "), "█"))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+4, 4+2*BOARD_SIZE_X, "█ Player   █"))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+4, 4+2*BOARD_SIZE_X+len(" Player   █ Targets:   X&X"), "█"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+4, 4+2*BOARD_SIZE_X+len(" Player   █ Target:    X  "), "█"))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (bolded_start+5, 4+2*BOARD_SIZE_X, "█▄▄▄▄▄▄▄▄▄▄█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"))
 	# Unbold the other player
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (regular_start+1, 4+2*BOARD_SIZE_X, "╭──────────┬────────────────╮"))
@@ -85,24 +88,34 @@ def changePlayerVisual(current_player): # Update the CLI when the current user h
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (regular_start+3, 4+2*BOARD_SIZE_X, "│          │"))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (regular_start+3, 4+2*BOARD_SIZE_X+len("          │ CrazyFlie: X  "), "│"))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (regular_start+4, 4+2*BOARD_SIZE_X, "│          │"))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (regular_start+4, 4+2*BOARD_SIZE_X+len("          │ Targets:   X&X"), "│"))
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (regular_start+4, 4+2*BOARD_SIZE_X+len("          │ Target:    X  "), "│"))
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (regular_start+5, 4+2*BOARD_SIZE_X, "╰──────────┴────────────────╯"))
 	sys.stdout.flush()
 
 def printStatus(status): # Print status message (message length up to 130 chars)
 	if len(status) < 130:
 		status += " "*(130-len(status))
-	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 5+2*BOARD_SIZE_Y, 2, status[:130]))
+	if BOARD_SIZE_Y < 10:
+		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 21, 2, status[:130]))
+	else:
+		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 5+2*BOARD_SIZE_Y, 2, status[:130]))
 	sys.stdout.flush()
 
 def updateInfo(info_type, new_value): # Update the requested value at the CLI info box
 	if (info_type == INFO_STATUS) or (info_type == INFO_TURNS) or (info_type == INFO_ELAPSED):
 		if len(new_value) < 13:
 			new_value += " "*(13-len(new_value))
-		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 5+info_type,21+2*BOARD_SIZE_X, new_value[:13]))
+		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % ( 4+info_type,21+2*BOARD_SIZE_X, new_value[:13]))
 	else:
 		return
 	sys.stdout.flush() # Don't flush for invalid calls
+
+def drawPiece(board, old_pos = None): # Draw the drones position to the screen
+	if old_pos:
+		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (2+2*(BOARD_SIZE_Y-old_pos[1]), 1+2*(BOARD_SIZE_X-old_pos[0]), " "))
+	for piece,position in board.iteritems():
+		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (2+2*(BOARD_SIZE_Y-position[1]), 1+2*(BOARD_SIZE_X-position[0]), piece[0]))
+	sys.stdout.flush()
 
 def key_handle(): # Get one char from the keyboard
 	import termios
