9c0971ed134ec00d0c3e1ddeb6359a267a432512
xqgex <xqgex.n@gmail.com>
2018-08-10 23:45:02 +0300

    Switch from spaces to tabsgit status!

--- Windows_PC/dronesControllerTester.py
+++ Windows_PC/dronesControllerTester.py
@@ -3,31 +3,27 @@
 import logger, time
 from dronesControllerAPI import DronesControllerAPI
 
-cf_logger = logger.get_logger(__name__)  # debug(), info(), warning(), error(), exception(), critical()
+cf_logger = logger.get_logger(__name__) # debug(), info(), warning(), error(), exception(), critical()
 
-COMMANDS = ["crazyflie3$Register", "crazyflie3$TakeOff$3$3", "crazyflie3$UP", "crazyflie3$UP", "crazyflie3$LEFT",
-            "crazyflie3$LEFT", "crazyflie3$Land", "crazyflie3$UnRegister"]
-
+COMMANDS = ["crazyflie3$Register", "crazyflie3$TakeOff$3$3", "crazyflie3$UP", "crazyflie3$UP", "crazyflie3$LEFT", "crazyflie3$LEFT", "crazyflie3$Land", "crazyflie3$UnRegister"]
 
 def main():
-    dronesController = DronesControllerAPI()  # Optional variables: "ip", "port" and "buffer_size"
-    if not dronesController.connect():  # Return True on success and False otherwise
-        cf_logger.critical("Communication error")
-        exit(0)
-    for command in COMMANDS:
-        loop_status = dronesController.send(
-            command)  # Return 0 on success, 1 if the VM report on an error and -1 if the connection is closed
-        if loop_status == 1:
-            cf_logger.error("dronesControllerTester: Failed to execute command: {}".format(command))
-        elif loop_status == -1:
-            cf_logger.critical("Communication error")
-            exit(0)
-        time.sleep(1)
-    dronesController.disconnect()
-
+	dronesController = DronesControllerAPI() # Optional variables: "ip", "port" and "buffer_size"
+	if not dronesController.connect(): # Return True on success and False otherwise
+		cf_logger.critical("Communication error")
+		exit(0)
+	for command in COMMANDS:
+		loop_status = dronesController.send(command) # Return 0 on success, 1 if the VM report on an error and -1 if the connection is closed
+		if loop_status == 1:
+			cf_logger.error("dronesControllerTester: Failed to execute command: {}".format(command))
+		elif loop_status == -1:
+			cf_logger.critical("Communication error")
+			exit(0)
+		time.sleep(1)
+	dronesController.disconnect()
 
 if __name__ == "__main__":
-    cf_logger.info("######################################################")
-    cf_logger.info("####                   Started                    ####")
-    cf_logger.info("######################################################")
-    main()
+	cf_logger.info("######################################################")
+	cf_logger.info("####                   Started                    ####")
+	cf_logger.info("######################################################")
+	main()
--- Windows_PC/logger.py
+++ Windows_PC/logger.py
@@ -1,41 +1,34 @@
 import logging, os, sys
 from logging.handlers import TimedRotatingFileHandler
 
-FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+DEFAULT_DEBUG_LEVEL = logging.DEBUG
+FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s") # Set the logging format
 LOG_FOLDER = "log"
 LOG_FILE = "{}/crazy_game.log".format(LOG_FOLDER)
 
-DEFAULT_DEBUG_LEVEL = logging.DEBUG
-
-
 def get_console_handler():
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(FORMATTER)
-    return console_handler
-
+	console_handler = logging.StreamHandler(sys.stdout)
+	console_handler.setFormatter(FORMATTER)
+	return console_handler
 
 def get_file_handler():
-    if not os.path.isdir(LOG_FOLDER):
-        os.mkdir(LOG_FOLDER)
-    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight', utc=True)
-    file_handler.setFormatter(FORMATTER)
-    return file_handler
-
+	if not os.path.isdir(LOG_FOLDER):
+		os.mkdir(LOG_FOLDER)
+	file_handler = TimedRotatingFileHandler(LOG_FILE, when="midnight", utc=True)
+	file_handler.setFormatter(FORMATTER)
+	return file_handler
 
 def set_default_debug_level(logging_level):
-    global DEFAULT_DEBUG_LEVEL
-    DEFAULT_DEBUG_LEVEL = logging_level
+	global DEFAULT_DEBUG_LEVEL
+	DEFAULT_DEBUG_LEVEL = logging_level
 
-
-def get_logger(logger_name, logging_level=None):
-    logger = logging.getLogger(logger_name)  # debug(), info(), warning(), error(), exception(), critical()
-    logging_level = DEFAULT_DEBUG_LEVEL if not logging_level else logging_level
-    logger.setLevel(logging_level)
-    logger.addHandler(get_console_handler())
-    logger.addHandler(get_file_handler())
-    logger.propagate = False  # With this pattern, it's rarely necessary to propagate the error up to parent
-    return logger
-
+def get_logger(logger_name, logging_level=DEFAULT_DEBUG_LEVEL):
+	logger = logging.getLogger(logger_name) # debug(), info(), warning(), error(), exception(), critical()
+	logger.setLevel(logging_level)
+	logger.addHandler(get_console_handler())
+	logger.addHandler(get_file_handler())
+	logger.propagate = False # With this pattern, it's rarely necessary to propagate the error up to parent
+	return logger
 
 if __name__ == "__main__":
-    print("This is not the way to do it...")
+	print("This is not the way to do it...")
