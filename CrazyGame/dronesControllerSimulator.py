4d2dad8f8e99f5757283400a334cbcf51f654ef5
tomer <tomer.kalish91@gmail.com>
2018-08-19 20:36:17 +0300

    some fixes

--- CrazyGame/dronesControllerSimulator.py
+++ CrazyGame/dronesControllerSimulator.py
@@ -31,40 +31,42 @@ class DronesController:
         return self.objects.keys()
 
     def get_object_position(self, object_name):
-        if not self.objects[object_name].on_the_go:
-            return self.objects[object_name].pos
+        object = self.objects[object_name]
+        if not object.on_the_go:
+            return object.pos
 
-        diff = time.time() - self.objects[object_name].start_time
+        diff = time.time() - object.start_time
         if diff >= 1:
-            self.objects[object_name].on_the_go = False
-            return self.objects[object_name].pos
+            object.on_the_go = False
+            return object.pos
 
-        return ((self.objects[object_name].pos[0]-self.objects[object_name].start_pos[0])*diff,
-                 (self.objects[object_name].pos[1]-self.objects[object_name].start_pos[1])*diff,
-                self.objects[object_name].pos[2])
+        return ( (object.pos[0] - object.start_pos[0]) * diff,
+                 (object.pos[1] - object.start_pos[1]) * diff,
+                  object.pos[2] )
 
     def move_drone(self, drone_name, direction_vector): # direction_vector = [x, y]
-        self.objects[drone_name].start_pos = self.get_object_position(drone_name)
-        self.objects[drone_name].start_time = time.time()
-        self.objects[drone_name].on_the_go = True
-        self.objects[drone_name].pos = (self.objects[drone_name].start_pos[0] + direction_vector[0]*SPEED,
-                                        self.objects[drone_name].start_pos[1] + direction_vector[1]*SPEED,
-                                        self.objects[drone_name].start_pos[2])
+        drone = self.objects[drone_name]
+        drone.start_pos = self.get_object_position(drone_name)
+        drone.start_time = time.time()
+        drone.on_the_go = True
+        drone.pos = (drone.start_pos[0] + direction_vector[0]*SPEED,
+                     drone.start_pos[1] + direction_vector[1]*SPEED,
+                     drone.start_pos[2])
 
     def goto(self, drone_name, pos): # pos = [x, y]
-        self.objects[drone_name] = (pos[0],
-                                    pos[1],
-                                    self.objects[drone_name][2])
+        self.objects[drone_name].pos = (pos[0],
+                                        pos[1],
+                                        self.objects[drone_name][2])
 
     def take_off(self, drone_name):
-        self.objects[drone_name] = (self.objects[drone_name][0],
-                                    self.objects[drone_name][1],
-                                    TAKEOFF_HEIGHT)
+        self.objects[drone_name].pos = (self.objects[drone_name][0],
+                                        self.objects[drone_name][1],
+                                        TAKEOFF_HEIGHT)
 
     def land(self, drone_name):
-        self.objects[drone_name] = (self.objects[drone_name][0],
-                                    self.objects[drone_name][1],
-                                    0)
+        self.objects[drone_name].pos = (self.objects[drone_name][0],
+                                        self.objects[drone_name][1],
+                                        0)
 
     def battery_status(self, drone_name):
         return 0
