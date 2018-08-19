94ac5769e59a86ad3a5c4936d296e1c7ed660bcd
= <matan.harel.mh@gmail.com>
2018-08-19 17:53:42 +0300

    new drones controller api

--- CrazyGame/dronesController.py
+++ CrazyGame/dronesController.py
@@ -13,7 +13,7 @@ DEFAULT_BUFFER_SIZE = 1024
 CONNECTION_TIME_OUT = 1
 
 
-class DronesController(object):
+class DronesController:
     def __init__(self, ip=VM_IP, port=DEFAULT_TCP_PORT, buffer_size=DEFAULT_BUFFER_SIZE):
         self._tcp_ip = ip
         self._tcp_port = port
@@ -37,33 +37,67 @@ class DronesController(object):
                             retry + 1, number_of_trials, self._tcp_ip, self._tcp_port))
         raise ConnectionError
 
-    def send(self, command):  # Return 0 on success, 1 if the Vm report on an error and -1 if the connection is closed
-        try:
-            self._socket.send(command.encode())
-        except socket.error as e:
-            cf_logger.critical("Socket error: {}".format(e))
-            return -1
-        try:
-            data = self._socket.recv(self._buffer_size).decode()
-            if data == "OK":
-                cf_logger.debug("Send: '{}' received: '{}'".format(command, data))
-                return 0
-            elif data == "FAILED":
-                cf_logger.error("Executing '{}' failed".format(command))
-            elif data == "FORMAT":
-                cf_logger.error("Command '{}' rejected by the VM - Invalid format".format(command))
-            elif data == "INVALID":
-                cf_logger.error("Command '{}' rejected by the VM - Invalid parameters".format(command))
-            elif len(data) == 0:
-                cf_logger.critical("The client at the VM died")
-                return -1
-            else:
-                cf_logger.error("Unknown response '{}' from the VM for '{}'".format(data, command))
-        except socket.timeout:
-            cf_logger.critical("Timeout")
-            return -1
-        return 1
-
     def disconnect(self):
-        cf_logger.info('disconnect')
         self._socket.close()
+###################################################################################################
+
+    def get_world_size(self):
+        x = y = 0
+        return [x, y]
+
+
+    def get_objects(self):
+        return ["names_0", "names_1", "names_2"]
+
+
+    def get_object_position(self, object_name):
+        x = y = z = 0
+        return [x, y, z]
+
+
+    def move_drone(self, drone_name, direction_vector): # direction_vector = [x, y]
+        pass
+
+
+    def goto(self, drone, pos): # pos = [x, y]
+        pass
+
+
+    def take_off(self, drone):
+        pass
+
+
+    def land(self, drone):
+        pass
+
+
+    def battery_status(self, drone):
+        return 0
+###################################################################################################
+#	def _send(self, command): # Return 0 on success, 1 if the Vm report on an error and -1 if the connection is closed
+#		try:
+#			self._socket.send(command.encode())
+#		except socket.error as e:
+#			cf_logger.critical("Socket error: {}".format(e))
+#			return -1
+#		try:
+#			data = self._socket.recv(self._buffer_size).decode()
+#			if data == "OK":
+#				cf_logger.debug("Send: '{}' received: '{}'".format(command, data))
+#				return 0
+#			elif data == "FAILED":
+#				cf_logger.error("Executing '{}' failed".format(command))
+#			elif data == "FORMAT":
+#				cf_logger.error("Command '{}' rejected by the VM - Invalid format".format(command))
+#			elif data == "INVALID":
+#				cf_logger.error("Command '{}' rejected by the VM - Invalid parameters".format(command))
+#			elif len(data) == 0:
+#				cf_logger.critical("The client at the VM died")
+#				return -1
+#			else:
+#				cf_logger.error("Unknown response '{}' from the VM for '{}'".format(data, command))
+#		except socket.timeout:
+#			cf_logger.critical("Timeout")
+#			return -1
+#		return 1
+###################################################################################################
