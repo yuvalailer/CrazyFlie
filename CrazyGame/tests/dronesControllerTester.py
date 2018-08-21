c67edfe8df8c37fcfac91a8b3a37c9543ac5c0c0
exeaction <Crazy@game.com>
2018-08-21 10:38:46 +0300

    just test commit

--- CrazyGame/tests/dronesControllerTester.py
+++ CrazyGame/tests/dronesControllerTester.py
@@ -8,6 +8,7 @@ from CrazyGame.dronesController import DronesController
 
 cf_logger = logger.get_logger(__name__) # debug(), info(), warning(), error(), exception(), critical()
 
+drone_name = "crazyflie2"
 def main():
 	drones_controller = DronesController() # Optional variables: "ip", "port" and "buffer_size"
 	if not drones_controller.connect(number_of_trials=5, time_between_trails=3):
@@ -16,12 +17,13 @@ def main():
 	drones_list = drones_controller.get_objects()
 	cf_logger.info("drones_list: {}".format(drones_list))
 	cf_logger.info("get_world_size: {}".format(drones_controller.get_world_size()))
-	cf_logger.info("battery_status: {}".format(drones_controller.battery_status("crazyflie2")))
-	drones_controller.take_off("crazyflie2")
-	cf_logger.debug("get_object_position: {}".format(drones_controller.get_object_position("crazyflie2")))
-	drones_controller.goto("crazyflie2", [1,1])
-	cf_logger.debug("get_object_position: {}".format(drones_controller.get_object_position("crazyflie2")))
-	drones_controller.land("crazyflie2")
+	cf_logger.info("battery_status: {}".format(drones_controller.battery_status(drone_name)))
+	drones_controller.take_off(drone_name)
+	drones_controller.battery_status(drone_name)
+	cf_logger.debug("get_object_position: {}".format(drones_controller.get_object_position(drone_name)))
+	drones_controller.goto(drone_name, [1,1])
+	cf_logger.debug("get_object_position: {}".format(drones_controller.get_object_position(drone_name)))
+	drones_controller.land(drone_name)
 	drones_controller.disconnect()
 
 if __name__ == "__main__":
